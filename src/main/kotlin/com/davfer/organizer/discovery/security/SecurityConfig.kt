23e5a936d128e50a7b399bc3ebdc9c380a018abb
David Ferreira <mrjdavidfg@gmail.com>
2019-01-02 21:14:37 -0500

    * Added security and session managment.

---
+++ src/main/kotlin/com/davfer/organizer/discovery/security/SecurityConfig.kt
@@ -0,0 +1,36 @@
+package com.davfer.organizer.discovery.security
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+
+
+@Configuration
+@EnableWebSecurity
+@Order(1) //Use this configuration as first priority
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+    @Autowired
+    fun configureGlobal(auth: AuthenticationManagerBuilder) {
+        auth
+                .inMemoryAuthentication()
+                .withUser("discoveryAdmin")
+                .password("discoveryPassword12345")
+                .roles("SYSTEM")
+    }
+
+    override fun configure(http: HttpSecurity) {
+        http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS) //Session will be created on every login attempt
+                .and().requestMatchers().antMatchers("/eureka/**")
+                .and().authorizeRequests().antMatchers("/eureka/**")
+                .hasRole("SYSTEM").anyRequest().denyAll().and()
+                .httpBasic().and().csrf().disable()
+    }
+}
